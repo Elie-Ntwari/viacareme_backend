58744c9f410c9584b58bef4aaf8a77f64e97c472
elie_ntwari <elientwari@gmail.com>
2025-09-25 11:34:25 +0100

    Add put et delete hospital

--- hospital_module/views.py
+++ hospital_module/views.py
@@ -8,6 +8,51 @@ from hospital_module.services import HopitalService
 
 
 class HopitalViewSet(viewsets.ViewSet):
+    @action(detail=True, methods=['put', 'patch'], url_path='update')
+    def update_hopital(self, request, pk=None):
+        """PUT/PATCH /api/hospitals/{id}/update/ - Modifier un hôpital"""
+        hopital = HopitalService.get_hopital(pk)
+        if isinstance(hopital, Response):
+            return hopital
+        serializer = CreateHopitalSerializer(data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        # Mise à jour des champs hôpital
+        for attr, value in serializer.validated_data.items():
+            if attr != "gestionnaire":
+                setattr(hopital, attr, value)
+        hopital.save()
+
+        # Mise à jour du gestionnaire principal si fourni
+        gestionnaire_data = serializer.validated_data.get("gestionnaire")
+        if gestionnaire_data:
+            gestionnaire = hopital.gestionnaires.filter(is_admin_local=True).first()
+            if gestionnaire:
+                user = gestionnaire.user
+                # Met à jour les champs du user
+                for field in ["nom", "postnom", "prenom", "email", "telephone"]:
+                    if field in gestionnaire_data:
+                        setattr(user, field, gestionnaire_data[field])
+                user.save()
+                # Met à jour is_admin_local si fourni
+                if "is_admin_local" in gestionnaire_data:
+                    gestionnaire.is_admin_local = gestionnaire_data["is_admin_local"]
+                    gestionnaire.save()
+        return Response({"message": "Hôpital et gestionnaire modifiés avec succès."}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['delete'], url_path='delete')
+    def delete_hopital(self, request, pk=None):
+        """DELETE /api/hospitals/{id}/delete/ - Supprimer un hôpital et son gestionnaire"""
+        hopital = HopitalService.get_hopital(pk)
+        if isinstance(hopital, Response):
+            return hopital
+        # Supprimer tous les gestionnaires liés à l'hôpital
+        gestionnaires = hopital.gestionnaires.all()
+        for gestionnaire in gestionnaires:
+            user = gestionnaire.user
+            gestionnaire.delete()
+            user.delete()
+        hopital.delete()
+        return Response({"message": "Hôpital et tous ses gestionnaires supprimés avec succès."}, status=status.HTTP_200_OK)
     permission_classes = [IsSuperAdmin]  # Permission par défaut
   
 
